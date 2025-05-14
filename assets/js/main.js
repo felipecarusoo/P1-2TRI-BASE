35119a94ddc27436ed2f3611bf0e96a78cf3a01f
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-05-14 10:07:19 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxwyz'; 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
-const simbolos = '!@%*ç?#-_;,.></{}()';
-const numeroSenha = document.querySelector('.parametro-senha__texto')
-
-
+const simbolos = '!@%*?';
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
